27399c994f6fae22eb617225cfdac3b9cf020968
JossPato <213367@ids.upchiapas.edu.mx>
2022-09-30 13:05:01 -0500

    First commit b

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:Jose0Angel0Nataren/proyecto02/vite-project-4.git main:gh-pages
+# git push -f git@github.com:Jose0Angel0Nataren/webapp.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], publicDir:true,
-  base:'/proyecto02/vite-project-4'
+  plugins: [react()], 
+  publicDir:true,
+  base:'/webapp/'
 })
